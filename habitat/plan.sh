9d9ec61c9b15396b89c0d868412d9782b63cfa09
danuf <33052960+danuf@users.noreply.github.com>
2018-08-10 17:42:34 +0300

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,12 @@
-pkg_name=simple-jar
-pkg_origin=danuf
-pkg_version="0.0.1"
+pkg_name=ptr_service
+pkg_origin=mfol
+pkg_version="0.1.0"
+pkg_filename="price-type-rules-service-1.0-SNAPSHOT.jar"
 pkg_deps=(core/jre8 core/maven)
+pkg_exports=(
+  [port]=server.port
+)
+pkg_exposes=(port)
 
 do_build() {
   mvn clean install -DskipTests=true
@@ -10,5 +15,5 @@ do_build() {
 do_install() {
   local app_path="$pkg_prefix/app"
   mkdir -p $app_path
-  cp -R /src/target/rd-1.0-SNAPSHOT.jar $app_path/
+  cp -R "/src/target/$pkg_filename" $app_path/
 }
